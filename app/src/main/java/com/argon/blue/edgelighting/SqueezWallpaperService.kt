a0a87aa97b3e6d7771b5d388d05e4a3007574b18
sean <emotionfxxk@gmail.com>
2023-12-11 22:39:19 +0800

    Add Squeez

--- app/src/main/java/com/argon/blue/edgelighting/SqueezWallpaperService.kt
+++ app/src/main/java/com/argon/blue/edgelighting/SqueezWallpaperService.kt
@@ -24,19 +24,25 @@ class SqueezWallpaperService  : WallpaperService(){
         private val handler = android.os.Handler();
         private val drawRunner = Runnable { draw() }
 
+        private var gradient: SweepGradient? = null
         var mSqueezebarPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             color = Color.parseColor("#FF0000")
             style = Paint.Style.FILL_AND_STROKE
             strokeWidth = 20f
+
         }
 
         var horizontalWidth = 0
         var surfaceWidth = 0
         var surfaceHeight = 0
-        var horizontalSqueezHeight = 20f
+        var horizontalSqueezHeight = 10f
+
+        var verticalSqueezWidth = 10f
+        var verticalHeight = 0
         var isClockwise = true
 
         var mSqueezebarAnimator: ValueAnimator? = null
+        private var mSqueezebarVerticalDisplacement = 0f
         private var mSqueezebarDisplacement = 0f
             set(value) {
                 field = value
@@ -84,17 +90,39 @@ class SqueezWallpaperService  : WallpaperService(){
             canvas.drawRect(rectBottom, mSqueezebarPaint)
         }
 
+        fun drawVerticalSqueezebar(canvas: Canvas, centerPositionY: Float, squeezeFactor: Float) {
+            // 1. draw right vertical squeez bar
+            val topPositionY = centerPositionY - squeezeFactor*verticalHeight/2f
+            val bottomPositionY = centerPositionY + squeezeFactor*verticalHeight/2f
+            val rectRight = RectF().apply {
+                left = surfaceWidth - verticalSqueezWidth
+                top = topPositionY
+                right = surfaceWidth.toFloat()
+                bottom = bottomPositionY
+            }
+            canvas.drawRect(rectRight, mSqueezebarPaint)
+            // 2. draw left vertical squeez bar
+            val leftRight = RectF().apply {
+                top = surfaceHeight - bottomPositionY
+                bottom = surfaceHeight - topPositionY
+                right = verticalSqueezWidth
+                left = 0f
+            }
+            canvas.drawRect(leftRight, mSqueezebarPaint)
+        }
+
         private fun draw() {
             val holder = surfaceHolder
             var canvas: Canvas? = null
             canvas = holder.lockCanvas()
             canvas.drawColor(Color.BLACK)
             val currentCenterPositionX = mSqueezebarDisplacement
+            val currentCenterPositionY = mSqueezebarVerticalDisplacement
             val squeezeFactor = if(mAnimationFraction <= 0.5) mAnimationFraction else 1-mAnimationFraction
             Log.d("HAHA", "squeezeFactor=${squeezeFactor}, " +
                     "mAnimationFraction=${mAnimationFraction}" + ",mSqueezebarDisplacement=${mSqueezebarDisplacement}")
             drawHorizontalSqueezebar(canvas, currentCenterPositionX, squeezeFactor)
-
+            drawVerticalSqueezebar(canvas, currentCenterPositionY, squeezeFactor)
             if (canvas != null)
                 holder.unlockCanvasAndPost(canvas)
         }
@@ -124,15 +152,26 @@ class SqueezWallpaperService  : WallpaperService(){
             surfaceWidth = width
             surfaceHeight = height
             horizontalWidth = width
-            mSqueezebarAnimator = ValueAnimator.ofFloat(0f, horizontalWidth.toFloat()).apply {
+            verticalHeight = height
+
+            gradient = SweepGradient(surfaceWidth.toFloat() / 2,
+                surfaceHeight.toFloat() / 2, COLORS2, null).apply {
+                //setLocalMatrix(gradientMatrix)
+            }
+            mSqueezebarPaint.shader = gradient
+
+            mSqueezebarAnimator = ValueAnimator.ofFloat(0f, /*horizontalWidth.toFloat()*/1f).apply {
                 addUpdateListener {
-                    mSqueezebarDisplacement = (it.animatedValue as Float)
+                    //mSqueezebarDisplacement = (it.animatedValue as Float)
+                    mSqueezebarDisplacement = (it.animatedValue as Float) * horizontalWidth.toFloat()
+                    mSqueezebarVerticalDisplacement = (it.animatedValue as Float) * verticalHeight.toFloat()
                     mAnimationFraction = it.animatedFraction
                 }
                 duration = 1000L
                 repeatMode = ValueAnimator.RESTART
                 repeatCount = ValueAnimator.INFINITE
-                interpolator = EaseInOutCubicInterpolator()
+                //interpolator = EaseInOutCubicInterpolator()
+                interpolator = LinearInterpolator()
                 start()
             }
         }
